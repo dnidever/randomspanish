ac00b189f3b90b50ca43b16a2f98de2857eb4a82
David Nidever <dnidever@montana.edu>
2025-08-04 20:25:53 -0600

    updates

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import shutil
 from setuptools import setup, find_packages, find_namespace_packages
 from setuptools.command.install import install
 
-setup(name='randomspanish',
+setup(name='randospanish',
       version='1.0.0',
       description='Spanish learning app',
       author='David Nidever',
       author_email='dnidever@montana.edu',
-      url='https://github.com/dnidever/randomspanish',
+      url='https://github.com/dnidever/randospanish',
       requires=['numpy','dlnpyutils'],
       zip_safe = False,
       include_package_data=True,

dce68b193558f579c3cc75f39c81f5b58d32088e
David Nidever <dnidever@montana.edu>
2025-08-06 16:36:16 -0600

    added pygame requirements

--- python/randospanish/utils.py
+++ python/randospanish/utils.py
@@ -1,4 +1,5 @@
 import os
+import pygame
 
 def datadir():
     """ Return the data/ directory."""
@@ -6,3 +7,18 @@ def datadir():
     codedir = os.path.dirname(fil)
     datadir = os.path.join(codedir,'data')+'/'
     return datadir
+
+def play_mp3_pygame(file_path):
+    """
+    Plays an MP3 file using Pygame.
+
+    Args:
+        file_path (str): The path to the MP3 file.
+    """
+    pygame.mixer.init()  # Initialize the mixer module
+    pygame.mixer.music.load(file_path)  # Load the MP3 file
+    pygame.mixer.music.play()  # Start playing the music
+
+    # Keep the program running until the music finishes or is explicitly stopped
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10) # Control CPU usage
